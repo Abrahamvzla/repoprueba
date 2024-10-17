7e0db016017c147d5a55a2435fe80ed0a89df411
Abraham Gómez <moisesdelciervo@gmail.com>
2024-10-17 14:45:04 -0400

    cambios e documentacion y main

--- main.py
+++ main.py
@@ -1,75 +1,89 @@
 from fastapi import FastAPI, Request, Response, Body
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import HTMLResponse
-from dto import ApiRequest
+from fastapi.responses import HTMLResponse,JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional
 import json
+
 app = FastAPI(
-    title="Sistema de Beneficios",
+    title="CURSO PRUEBA",
     version="1.0"
 )
-#app.tittle = "Sistema de Beneficios"
-#app.version = "0.1"
+
+class Beneficio(BaseModel):
+    id: Optional[int] = None
+    categoria: str = Field(default="BENEFICIO",min_length=4, max_length=10)
+    fecha: str = Field(default="00-00-2024",min_length=10, max_length=10)
+    empleados: int = Field(default=1,ge=1)
 
 beneficios = [
     {
-        "id":1,
+        "id": 1,
         "categoria": "BOLSA DE ALIMENTACIÓN",
         "fecha": "14-10-2024",
-        "empleados":20
+        "empleados": 20
     },
-     {
-        "id":2,
+    {
+        "id": 2,
         "categoria": "BOLSA DE PROTEINA",
         "fecha": "15-10-2024",
-        "empleados":20
-    } 
+        "empleados": 20
+    }
 ]
+
 
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse("<H1>Hola Mundo</H1>")
-    #return {"Nombre":"Abraham"}
-    #return {"code": "hola  acabas de nacer"}
 
-@app.post('/personas/{persona_id}')
-async def solicitar(request: Request):
-    data=await request.json()
-    print(data)
-    return Response(status_code=200, content=json.dumps(data), headers={"Content-Type":"application/json"})
+#@app.post('/personas/{persona_id}')
+#async def solicitar(request: Request):
+#    data = await request.json()
+#    print(data)
+#    return Response(status_code=200, content=json.dumps(data), headers={"Content-Type": "application/json"})
 
 @app.get('/beneficios', tags=['beneficios'])
-def get_benefcios():
-    return beneficios
-
+def obtener_lista_beneficios():
+    return JSONResponse(content=beneficios)
 
 @app.get('/beneficios/{id}', tags=['beneficios'])
-def get_beneficos(id: int):
+def obtener_un_benefico(id: int):
     for item in beneficios:
         if item["id"] == id:
-            return item
-    return []
-
-
-#@app.get('/beneficios/', tags=['beneficios'])
-#def get_benefcios_por_fecha(fecha: str):
-#    return fecha
+            #return item
+            return JSONResponse(content=item)
+    return JSONResponse([])
+    #return []
 
 @app.get('/beneficios/', tags=['beneficios'])
 def get_benefcios_por_fecha(fecha: str):
-    return [item for item in beneficios if item['fecha'] == fecha]
-#######
+    data = [item for item in beneficios if item['fecha'] == fecha]
+    return JSONResponse(content=data)
+    #return [item for item in beneficios if item['fecha'] == fecha]
+
 @app.post('/beneficios', tags=['beneficios'])
-async def crear_beneficio(request: ApiRequest):
-    data = request
+async def crear_beneficio(request: Request):
+    data = await request.json()
     beneficios.append(jsonable_encoder(data))
     return beneficios
 
-@app.put('/beneficios/{id}',tags=['beneficios'])
-def update_beneficio(id: int, categoria: str= Body(), fecha: str=Body(), empleados: int=Body()):
+@app.post('/beneficios', tags=['beneficios'])
+def registrar_beneficios(beneficio: Beneficio):
+    #beneficios.append(beneficio.dict())
+    return beneficios
+
+@app.put('/beneficios/{id}', tags=['beneficios'])
+def update_beneficio(id: int, beneficio: Beneficio):
     for item in beneficios:
-        if item["id"]==id:
-            item['categoria']=categoria,
-            item['fecha']=fecha,
-            item['empleados']=empleados
+        if item["id"] == id:
+            item['categoria'] = Beneficio.categoria
+            item['fecha'] = Beneficio.fecha
+            item['empleados'] = Beneficio.empleados
             return beneficios
-        
+
+@app.delete('/beneficios/{id}', tags=['beneficios'])
+def delete_beneficios(id: int):
+    for item in beneficios:
+        if item["id"] == id:
+            beneficios.remove(item)
+            return beneficios
